d1756ef5724553402a0f8050b13177b7554300c7
adamx4566 <adamix.4566@gmail.com>
2025-09-10 17:15:26 +0200

    Add files via upload

---
+++ Python exercises/conditionalexpression.py
@@ -0,0 +1,17 @@
+
+num = 5
+a = 6
+b = 7
+age = 25
+temperature = 30
+user_role = "admin"
+
+# print("Positive" if num > 0 else "Negative")
+# result = "EVEN" if num % 2 == 0 else "ODD"
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+# status = "Adult" if age >= 18 else "Child"
+# weather = "HOT" if temperature > 20 else "COLD"
+access_level = "Full Access" if user_role == "admin" else "Limited Access"
+
+print(access_level)
