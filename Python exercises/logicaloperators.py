60394c7dbcb3c3b34ef64c19abe6a5221d1148d8
adamx4566 <adamix.4566@gmail.com>
2025-09-10 17:15:40 +0200

    Add files via upload

---
+++ Python exercises/logicaloperators.py
@@ -0,0 +1,29 @@
+# temp = 20
+# is_raining = False
+
+# if temp > 35 or temp < 0 or is_raining:
+#    print("The outdoor event is cancelled")
+# else:
+#    print("The outdoor event is still scheduled")
+
+temp = 20
+is_sunny = True
+
+if temp >= 28 and is_sunny:
+    print("It is HOT outside 🥵")
+    print("It is SUNNY ☀️")
+elif temp <= 0 and is_sunny:
+    print("It is COLD outside 🥶")
+    print("It is SUNNY ☀️")
+elif  28 > temp > 0 and is_sunny:
+    print("It is WARM outside 😀")
+    print("It is SUNNY ☀️")
+elif temp >= 28 and not is_sunny:
+    print("It is HOT outside 🥵")
+    print("It is CLOUDY ☁️")
+elif temp <= 0 and not is_sunny:
+    print("It is COLD outside 🥶")
+    print("It is CLOUDY ☁️")
+elif  28 > temp > 0 and not is_sunny:
+    print("It is WARM outside 😀")
+    print("It is CLOUDY ☁️")
