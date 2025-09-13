cc87b5c1bdc7b816665210b8abbabf007949eae8
adamx4566 <adamix.4566@gmail.com>
2025-09-13 18:37:01 +0200

    Add files via upload

---
+++ Python exercises/collections.py
@@ -0,0 +1,46 @@
+
+# fruits = ["apple", "orange", "banana", "coconut"]
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits)
+
+# fruits[0] = "pineapple"
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits.insert(0, "pineapple")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# print(fruits.index("apple"))
+# print(fruits.count("banana"))
+
+# print(fruits)
+# print(fruits[0])
+# for fruit in fruits:
+#     print(fruit)
+
+
+# fruits = {"apple", "orange", "banana", "coconut"}
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits)
+
+# fruits.add("pineapple")
+# fruits.remove("apple")
+# fruits.pop()
+# fruits.clear()
+
+# print(fruits)
+
+# fruits = ("apple", "orange", "banana", "coconut")
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits)
+
+# print(fruits.index("apple"))
+# print(fruits.count("coconut"))
+# for fruit in fruits:
+#     print(fruit)
